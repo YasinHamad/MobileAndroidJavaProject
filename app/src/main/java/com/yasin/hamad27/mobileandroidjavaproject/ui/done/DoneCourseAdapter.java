c554ef7a73225ef6fc0703af43aca84fdb4a1a63
fattouma7 <fattoumaflity7@gmail.com>
2026-01-25 11:14:44 +0200

    m

--- app/src/main/java/com/yasin/hamad27/mobileandroidjavaproject/ui/done/DoneCourseAdapter.java
+++ app/src/main/java/com/yasin/hamad27/mobileandroidjavaproject/ui/done/DoneCourseAdapter.java
@@ -1,4 +1,76 @@
 package com.yasin.hamad27.mobileandroidjavaproject.ui.done;
 
-public class DoneCourseAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yasin.hamad27.mobileandroidjavaproject.R;
+
+public class DoneCourseAdapter extends RecyclerView.Adapter<DoneCourseAdapter.MyViewHolder> {
+
+        String[] courseTitle;
+        String[] courseDescription;
+        int[] BtnCourseDelete;
+
+        public DoneCourseAdapter(String[] courseTitle, String[] courseDescription, int[] BtnCourseDelete) {
+            this.courseTitle = courseTitle;
+            this.courseDescription = courseDescription;
+            this.BtnCourseDelete = BtnCourseDelete;
+        }
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            private final TextView courseTitle;
+            private final TextView courseDescription;
+            private final ImageView BtnCourseDelete;
+
+            public MyViewHolder(@NonNull View view) {
+                super(view);
+                courseTitle = view.findViewById(R.id.doneTvCourseTitle);
+                courseDescription = view.findViewById(R.id.doneTvCourseDescription);
+                BtnCourseDelete = view.findViewById(R.id.doneBtnCourseDelete);
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+
+            public TextView getCourseTitle() {
+                return courseTitle;
+            }
+
+            public TextView getCourseDescription() {
+                return courseDescription;
+            }
+
+            public ImageView getCourseBtnDelete() {
+                return BtnCourseDelete;
+            }
+        }
+
+
+        @NonNull
+        @Override
+        public DoneCourseAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_done_lecture, parent, false);
+            return new DoneCourseAdapter.MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DoneCourseAdapter.MyViewHolder holder, int position) {
+            holder.getCourseTitle().setText(courseTitle[position]);
+            holder.getCourseDescription().setText(courseDescription[position]);
+            holder.getCourseBtnDelete().setImageResource(BtnCourseDelete[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return courseTitle.length;
+        }
 }
--- app/src/main/java/com/yasin/hamad27/mobileandroidjavaproject/ui/done/DoneLectureAdapter.java
+++ app/src/main/java/com/yasin/hamad27/mobileandroidjavaproject/ui/done/DoneLectureAdapter.java
@@ -1,4 +1,76 @@
 package com.yasin.hamad27.mobileandroidjavaproject.ui.done;
 
-public class DoneLectureAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yasin.hamad27.mobileandroidjavaproject.R;
+
+public class DoneLectureAdapter extends RecyclerView.Adapter<DoneLectureAdapter.MyViewHolder> {
+
+    String[] lectureTitle;
+    String[] courseName;
+    int[] BtnLectureDelete;
+
+    public DoneLectureAdapter(String[] lectureTitle, String[] courseName, int[] BtnLectureDelete) {
+        this.lectureTitle = lectureTitle;
+        this.courseName = courseName;
+        this.BtnLectureDelete = BtnLectureDelete;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        private final TextView lectureTitle;
+        private final TextView courseName;
+        private final ImageView BtnLectureDelete;
+
+        public MyViewHolder(@NonNull View view) {
+            super(view);
+            lectureTitle = view.findViewById(R.id.doneTvLectureTitle);
+            courseName = view.findViewById(R.id.doneTvCourseName);
+            BtnLectureDelete = view.findViewById(R.id.doneBtnLectureDelete);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        public TextView getLectureTitle() {
+            return lectureTitle;
+        }
+
+        public TextView getCourseName() {
+            return courseName;
+        }
+
+        public ImageView getLectureBtnDelete() {
+            return BtnLectureDelete;
+        }
+    }
+
+
+        @NonNull
+        @Override
+        public DoneLectureAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_done_lecture, parent, false);
+            return new DoneLectureAdapter.MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DoneLectureAdapter.MyViewHolder holder, int position) {
+            holder.getLectureTitle().setText(lectureTitle[position]);
+            holder.getCourseName().setText(courseName[position]);
+            holder.getLectureBtnDelete().setImageResource(BtnLectureDelete[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return lectureTitle.length;
+        }
 }
